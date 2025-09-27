9038c3221c669480b175f293d69a889b21b7616d
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-27 12:19:07 +0000

    Refactor transaction response to include status code

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Transaction endpoint
 app.post('/transaction', (req, res) => {
   // Process transaction
-  res.send('Transaction has been completed!');
+  res.status(200).send('Transaction has been completed!');
 });
 
 module.exports = app;
